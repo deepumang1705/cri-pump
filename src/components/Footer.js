dbdf011fb55914ee2709b0100422b9c24d6d1cdc
deepumang1705 <deepumang1705@gmail.com>
2020-09-13 13:47:23 +0530

    fontawesome icon added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
 import { Image } from 'react-bootstrap'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faGlobe, faPhone} from '@fortawesome/free-solid-svg-icons'
+import {faFacebook} from '@fortawesome/free-brands-svg-icons'
 
 
 function Footer() {
@@ -8,9 +11,9 @@ function Footer() {
         <footer className='text-light p-4 my-2 mx-2' style={{backgroundColor:'#ec3237'}}>
             <Container>
                 <Row>
-                    <Col xs={12} md={4}><Image src="callicon2.png"/>Toll free 1800 200 1234</Col>
-                    <Col xs={12} md={4}><Image src="fb3.png"/>www.facebook.com/cripumps</Col>
-                    <Col xs={12} md={4}><Image src="www.png"/>www.crigroups.com</Col>
+                    <Col xs={12} md={4}><FontAwesomeIcon icon={faPhone} size="2x"/>Toll free 1800 200 1234</Col>
+                    <Col xs={12} md={4}><FontAwesomeIcon icon={faFacebook} size="2x"/> www.facebook.com/cripumps</Col>
+                    <Col xs={12} md={4}><FontAwesomeIcon icon={faGlobe} size="2x"/> www.crigroups.com</Col>
                 </Row>
             </Container>
             </footer>
